0753258097b46f2bf7a6a11331454d15b0073189
Lucas <lucas13dealmeidamoraes@gmail.com>
2024-06-17 17:42:25 -0300

    added index static

---
+++ spring-boot-todo-application/src/main/java/com/timebomb/spring_boot_todo_application/controllers/TodoItemController.java
@@ -0,0 +1,19 @@
+package com.timebomb.spring_boot_todo_application.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TodoItemController {
+    private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        logger.debug("request to GET index");
+        return new ModelAndView("index.html");
+        //return "oi";
+    }
+}
