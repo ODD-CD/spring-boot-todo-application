c048a6ca0f02afbc126a28d7c8a08d00b0e88542
Lucas <lucas13dealmeidamoraes@gmail.com>
2024-06-24 15:00:29 -0300

    Update and delete forms

--- spring-boot-todo-application/src/main/java/com/timebomb/spring_boot_todo_application/controllers/TodoItemController.java
+++ spring-boot-todo-application/src/main/java/com/timebomb/spring_boot_todo_application/controllers/TodoItemController.java
@@ -1,12 +1,20 @@
 package com.timebomb.spring_boot_todo_application.controllers;
 
+import com.timebomb.spring_boot_todo_application.models.TodoItem;
 import com.timebomb.spring_boot_todo_application.repositories.TodoItemRepository;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.Instant;
 
 @Controller
 public class TodoItemController {
@@ -23,5 +31,16 @@ public class TodoItemController {
         //System.out.println("17/");
         return modelAndView;
         //return "Oi";
+    }
+
+    @PostMapping("/todo/{id}")
+    public String updateTodoItem(@PathVariable("id") long id, @Valid TodoItem todoItem, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
     }
 }
